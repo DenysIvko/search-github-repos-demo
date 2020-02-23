b414cbe5e0b915a756d572262da86e6090da6053
denys ivko <denys.ivko@external.blueapron.com>
2020-02-23 20:50:54 +0200

    fix plop templates

--- plopfile.js
+++ plopfile.js
@@ -14,19 +14,19 @@ module.exports = (plop) => {
     actions: [{
       type: 'add',
       path: 'src/components/{{properCase name}}/{{properCase name}}.js',
-      templateFile: 'utils/plop-templates/component/{{type}}.hbs'
+      templateFile: 'src/utils/plop-templates/component/{{type}}.hbs'
     }, {
       type: 'add',
       path: 'src/components/{{properCase name}}/__tests__/{{properCase name}}.spec.js',
-      templateFile: 'utils/plop-templates/component/component-spec.hbs'
+      templateFile: 'src/utils/plop-templates/component/component-spec.hbs'
     }, {
       type: 'add',
-      path: 'src/components/{{properCase name}}/{{properCase name}}.mscss',
-      templateFile: 'utils/plop-templates/component/css.hbs'
+      path: 'src/components/{{properCase name}}/{{properCase name}}.scss',
+      templateFile: 'src/utils/plop-templates/component/css.hbs'
     }, {
       type: 'add',
       path: 'src/components/{{properCase name}}/index.js',
-      templateFile: 'utils/plop-templates/component/index-export.hbs'
+      templateFile: 'src/utils/plop-templates/component/index-export.hbs'
     }],
   });
 
@@ -44,15 +44,15 @@ module.exports = (plop) => {
     actions: [{
       type: 'add',
       path: 'src/containers/{{properCase name}}/{{properCase name}}.js',
-      templateFile: 'utils/plop-templates/component/container.hbs'
+      templateFile: 'src/utils/plop-templates/component/container.hbs'
     }, {
       type: 'add',
       path: 'src/containers/{{properCase name}}/__tests__/{{properCase name}}.spec.js',
-      templateFile: 'utils/plop-templates/component/container-spec.hbs'
+      templateFile: 'src/utils/plop-templates/component/container-spec.hbs'
     }, {
       type: 'add',
       path: 'src/containers/{{properCase name}}/index.js',
-      templateFile: 'utils/plop-templates/component/index-export.hbs'
+      templateFile: 'src/utils/plop-templates/component/index-export.hbs'
     }],
   });
 
@@ -66,23 +66,23 @@ module.exports = (plop) => {
     actions: [{
       type: 'add',
       path: 'src/reducers/{{lowerCase name}}/{{lowerCase name}}.js',
-      templateFile: 'utils/plop-templates/reducer/reducer.hbs'
+      templateFile: 'src/utils/plop-templates/reducer/reducer.hbs'
     }, {
       type: 'add',
       path: 'src/reducers/{{lowerCase name}}/__tests__/{{lowerCase name}}.reducer.spec.js',
-      templateFile: 'utils/plop-templates/reducer/reducer-spec.hbs'
+      templateFile: 'src/utils/plop-templates/reducer/reducer-spec.hbs'
     }, {
       type: 'add',
       path: 'src/reducers/{{lowerCase name}}/__tests__/{{lowerCase name}}.action-creators.spec.js',
-      templateFile: 'utils/plop-templates/reducer/action-creators-spec.hbs'
+      templateFile: 'src/utils/plop-templates/reducer/action-creators-spec.hbs'
     }, {
       type: 'add',
       path: 'src/reducers/{{lowerCase name}}/__tests__/{{lowerCase name}}.selectors.spec.js',
-      templateFile: 'utils/plop-templates/reducer/selectors-spec.hbs'
+      templateFile: 'src/utils/plop-templates/reducer/selectors-spec.hbs'
     }, {
       type: 'add',
       path: 'src/reducers/{{lowerCase name}}/__tests__/{{lowerCase name}}.thunks.spec.js',
-      templateFile: 'utils/plop-templates/reducer/thunks-spec.hbs'
+      templateFile: 'src/utils/plop-templates/reducer/thunks-spec.hbs'
     }],
   });
 
@@ -96,11 +96,11 @@ module.exports = (plop) => {
     actions: [{
       type: 'add',
       path: 'src/utils/{{lowerCase name}}/{{lowerCase name}}.js',
-      templateFile: 'utils/plop-templates/generic/generic.hbs'
+      templateFile: 'src/utils/plop-templates/generic/generic.hbs'
     }, {
       type: 'add',
       path: 'src/utils/{{lowerCase name}}/__tests__/{{lowerCase name}}.spec.js',
-      templateFile: 'utils/plop-templates/generic/generic-spec.hbs'
+      templateFile: 'src/utils/plop-templates/generic/generic-spec.hbs'
     }],
   });
 };
